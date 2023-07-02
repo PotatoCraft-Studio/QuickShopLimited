e97f00fb8b610b90721c5bca5a6c8289d5f4643e
Florian Redl <88911666+RedlFlo@users.noreply.github.com>
2023-07-02 17:58:57 +0200

    Update QuickShopLimited.java (Click-Event)
    Added feature that players get the limit displayed in the chat when they click the store

--- src/main/java/studio/potatocraft/quickshoplimited/QuickShopLimited.java
+++ src/main/java/studio/potatocraft/quickshoplimited/QuickShopLimited.java
@@ -12,6 +12,7 @@ import org.maxgamer.quickshop.api.QuickShopAPI;
 import org.maxgamer.quickshop.api.command.CommandContainer;
 import org.maxgamer.quickshop.api.event.CalendarEvent;
 import org.maxgamer.quickshop.api.event.ShopPurchaseEvent;
+import org.maxgamer.quickshop.api.event.PlayerShopClickEvent;
 import org.maxgamer.quickshop.api.event.ShopSuccessPurchaseEvent;
 import org.maxgamer.quickshop.api.shop.Shop;
 import org.maxgamer.quickshop.util.MsgUtil;
@@ -62,6 +63,19 @@ public final class QuickShopLimited extends JavaPlugin implements Listener {
                 ChatColor.AQUA + MsgUtil.fillArgs(getConfig().getString("message.subtitle"), String.valueOf(limit - playerUsedLimit)));
     }
 
+    @EventHandler
+    public void shopClick(PlayerShopClickEvent event) {
+        Shop shop = event.getShop();
+        ConfigurationSection storage = shop.getExtra(this);
+        if (storage.getInt("limit") < 1) {
+            return;
+        }
+        int limit = storage.getInt("limit");
+        int playerUsedLimit = storage.getInt("data." + event.getPlayer().getUniqueId(), 0);
+        
+        event.getPlayer().sendMessage(ChatColor.ORANGE + MsgUtil.fillArgs(getConfig().getString("tell-the-limit"), String.valueOf(limit - playerUsedLimit), String.valueOf(event.getAmount())));
+    }
+    
     @EventHandler(ignoreCancelled = true)
     public void shopPrePurchase(ShopPurchaseEvent event) {
         Shop shop = event.getShop();
@@ -71,7 +85,7 @@ public final class QuickShopLimited extends JavaPlugin implements Listener {
         }
         int limit = storage.getInt("limit");
         int playerUsedLimit = storage.getInt("data." + event.getPlayer().getUniqueId(), 0);
-        if (playerUsedLimit + event.getAmount() > limit) {
+        if (playerUsedLimit + event.getAmount() == limit) {
             event.getPlayer().sendMessage(ChatColor.RED + MsgUtil.fillArgs(getConfig().getString("reach-the-limit"), String.valueOf(limit - playerUsedLimit), String.valueOf(event.getAmount())));
             event.setCancelled(true);
         }
