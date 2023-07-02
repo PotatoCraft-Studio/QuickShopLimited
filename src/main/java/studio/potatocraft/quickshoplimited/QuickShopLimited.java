8a24134f115cce28cd8feea07e218bce3b00b0d6
Florian Redl <88911666+RedlFlo@users.noreply.github.com>
2023-07-02 18:15:51 +0200

    Update QuickShopLimited.java

--- src/main/java/studio/potatocraft/quickshoplimited/QuickShopLimited.java
+++ src/main/java/studio/potatocraft/quickshoplimited/QuickShopLimited.java
@@ -85,7 +85,7 @@ public final class QuickShopLimited extends JavaPlugin implements Listener {
         }
         int limit = storage.getInt("limit");
         int playerUsedLimit = storage.getInt("data." + event.getPlayer().getUniqueId(), 0);
-        if (playerUsedLimit + event.getAmount() == limit) {
+        if (playerUsedLimit + event.getAmount() > limit) {
             event.getPlayer().sendMessage(ChatColor.RED + MsgUtil.fillArgs(getConfig().getString("reach-the-limit"), String.valueOf(limit - playerUsedLimit), String.valueOf(event.getAmount())));
             event.setCancelled(true);
         }
